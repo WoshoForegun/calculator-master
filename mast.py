73309b8d38b3c7b9233e4dffc195cd15a7e89288
WoshoForegun <tsuchekage@gmail.com>
2021-09-22 21:32:21 +0300

    calculator master

---
+++ mast.py
@@ -0,0 +1,28 @@
+x = int(input('Ч 1'))
+u = int(input('Ч 2'))
+
+def sum(a,b):
+    return a * b
+ 
+print(sum(x,u))
+
+p = int(input('Плюс 1'))
+s = int(input('Плюс 2'))
+
+def sum(f,z):
+    return f + z
+print(sum(p,s))
+
+q = int(input('Минус 1'))
+w = int(input('Минус 2'))
+
+def sum(t,y):
+    return t - y
+print(sum(q,w))
+
+o = int(input('Деление 1'))
+j = int(input('Деление 2'))
+
+def sum(l,k):
+    return l / k
+print(sum(o,j))
